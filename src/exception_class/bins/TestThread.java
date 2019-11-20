425940ac7e6f380b435c3112f8598405f2378e6a
marola <maroli.becker@outlook.com>
2019-11-20 14:52:50 -0300

    semáforo para threads: JOIN(temporizador). property isAlive, isto é, enquanto estivar viva a thread

--- src/exception_class/bins/TestThread.java
+++ src/exception_class/bins/TestThread.java
@@ -1,15 +1,37 @@
 package exception_class.bins;
 
-import exception_class.model.entities.MyThread;
+import exception_class.model.entities.MyRunnableThread;
 
 public class TestThread {
 
+	@SuppressWarnings("null")
 	public static void main(String[] args) {
-		
+		/*
 		MyThread myThread = new MyThread("Thread #1 - ", 2000);
 		MyThread myThread2 = new MyThread("Thread #2 ----- ", 1000);
 		MyThread myThread3 = new MyThread("Thread #3 ----------", 500);
+				*/
+		MyRunnableThread mrt1 = new MyRunnableThread("Runnable #1 ** ", 500);
+		MyRunnableThread mrt2 = new MyRunnableThread("Runnable #2 **** ", 500);
+		MyRunnableThread mrt3 = new MyRunnableThread("Runnable #3 ****** ", 500);
+		Thread th1 = new Thread(mrt1);
+		Thread th2 = new Thread(mrt2);
+		Thread th3 = new Thread(mrt3);
+		th1.start();
+		th2.start();
+		th3.start();
+		// em Java join é um semáforo para threads com temporizador.
+		try {
+			th1.join(500);
+			th2.join(500);
+			th3.join(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		
+		if(!(th1.isAlive() && (th2.isAlive()) && (th3.isAlive()))) {
+			System.out.println("***** Finish ******");
+		}
 	}
 
 }
---
+++ src/exception_class/model/entities/MyRunnableThread.java
@@ -0,0 +1,31 @@
+package exception_class.model.entities;
+
+public class MyRunnableThread implements Runnable {
+
+	private String nameThread;
+	private int timeSleep;
+	
+	public MyRunnableThread(String nameThread, int timeSleep) {
+		this.nameThread = nameThread;
+		this.timeSleep = timeSleep;
+	//	Thread thread = new Thread(this);
+	//	thread.start();
+	}
+
+	public String getNameThread() { return this.nameThread; }
+	
+	@Override
+	public void run() {
+		String name = this.getNameThread();
+		try {
+			for(int i=0; i<=5; i++) {
+				System.out.println(getNameThread() + " -> " + i);
+				Thread.sleep(timeSleep);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(name + " finish ");
+	}
+	
+}
